c6adf842099cfd11a7a3608d7804b7406d170480
wangfangchen <itjavawfc@163.com>
2023-01-28 21:56:39 +0800

    内置函数TODO

---
+++ first001/src/com/deling/demo/NothingTypeDemo.kt
@@ -0,0 +1,12 @@
+package com.deling.demo
+
+fun main() {
+    println(test(18,"方晨"))
+    TODO("报错测试")
+    println(" Hello !")
+
+}
+
+private fun test(age:Int,name:String){
+    println("$name 岁数 $age")
+}
